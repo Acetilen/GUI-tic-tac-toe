a79cba4d4ea34c73a478207cc765649b7a316f81
Миша <yerafeyeum@gmail.com>
2025-11-05 16:34:52 +0300

    2

--- main/main.py
+++ main/main.py
@@ -1,8 +1,8 @@
 from tkinter import *
 import numpy
 
-field = numpy.matrix("0 0 0; 0 0 0; 0 0 0")
-
+field = numpy.matrix("0 2 0; 0 0 0; 0 0 0")
+print(field[0,1])
 
 main = Tk()
 main.title("Tic-Tac-Toe")
