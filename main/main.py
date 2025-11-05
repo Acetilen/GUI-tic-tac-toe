d0423cd3d056799342b5030e4428cf39d380c82c
Миша <yerafeyeum@gmail.com>
2025-11-05 15:31:56 +0300

    1

--- main/main.py
+++ main/main.py
@@ -1,17 +1,12 @@
-import pandas as pd
+from tkinter import *
+import numpy
 
-data = pd.read_excel(r"C:\Users\stufs\Desktop\Git\GUI-tic-tac-toe\main\data.xlsx")
-d = list(data["A"])
-a = {}
-sum = 0
-for obj in d:
-    if obj not in list(a.keys()):
-        a.update({obj: 0})
-for obj in d:
-    for key in list(a.keys()):
-        if obj == key:
-            a[key] += 1
-for key in list(a.keys()):
-    sum += a[key]
-print(a)
-print(sum)
+field = numpy.matrix("0 0 0; 0 0 0; 0 0 0")
+
+
+main = Tk()
+main.title("Tic-Tac-Toe")
+
+
+
+# main.mainloop()
