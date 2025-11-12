91ad702466b55edf042b0d207da21a11737d4479
Миша <yerafeyeum@gmail.com>
2025-11-12 14:24:14 +0300

    начало самое

--- main/main.py
+++ main/main.py
@@ -1,14 +1,24 @@
 from tkinter import *
 import numpy
 
-field = numpy.matrix("0 2 0; 0 0 0; 0 0 0")
-turns = ["cross", "circle"]
+field = {
+    "choices":numpy.matrix("0 0 0; 0 0 0; 0 0 0"), 
+    "active":numpy.matrix("1 1 1; 1 1 1; 1 1 1")
+         }
+print(field["choices"])
+turns = {
+    1:"cross",
+    2:"circle"
+        }
 
-print(field[0,1])
+
+# Кнопки передают крест и нолик и позицию + меняется картинка на кнопке и она становиться не активной
+# После каждого нажатия на кнопку нужна проверка победы и сброс поля через пару секунд с промтом кто победил
 
 main = Tk()
 main.title("Tic-Tac-Toe")
+main.minsize(300,300)
 
 
-main.mainloop()
+# main.mainloop()
 
