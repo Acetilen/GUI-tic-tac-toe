b67fc33815b4c4df3802a6fd1cf17fbcde98e555
Миша <yerafeyeum@gmail.com>
2025-11-12 14:10:17 +0300

    a

--- main/main.py
+++ main/main.py
@@ -1,12 +1,14 @@
-# from tkinter import *
-# import numpy
+from tkinter import *
+import numpy
 
-# field = numpy.matrix("0 2 0; 0 0 0; 0 0 0")
-# turns = ["cross", "circle"]
+field = numpy.matrix("0 2 0; 0 0 0; 0 0 0")
+turns = ["cross", "circle"]
 
-# print(field[0,1])
+print(field[0,1])
 
-# main = Tk()
-# main.title("Tic-Tac-Toe")
+main = Tk()
+main.title("Tic-Tac-Toe")
 
+
+main.mainloop()
 
